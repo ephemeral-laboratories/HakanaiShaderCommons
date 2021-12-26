1d2e5227bc9dd420186779d9442ae2286ba22de0
Hakanai <hakanai@ephemeral.garden>
2021-12-26 18:59:05 +1100

    Editor scripts to create new shaders from templates

---
+++ Scripts/Editor/CreationMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+namespace HakanaiShaderCommons
+{
+    public static class CreationMenu
+    {
+        [MenuItem("Assets/Create/Shader/EL Raycast Shader", false, 100)]
+        public static void CreateRaycastShader()
+        {
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(
+                "Packages/garden.ephemeral.shader.commons/ShaderTemplates/RaycastShader.txt",
+                "New Raycast Shader.shader");
+        }
+
+        [MenuItem("Assets/Create/Shader/EL Raymarch Shader", false, 101)]
+        public static void CreateRaymarchShader()
+        {
+            // TODO
+        }
+
+        private static string GetSelectedAssetFolder()
+        {
+            if (Selection.assetGUIDs.Length == 0)
+            {
+                return "Assets";
+            }
+            
+            // May return the folder, may return the list of files selected in the folder
+            var path = AssetDatabase.GUIDToAssetPath(Selection.assetGUIDs[0]);
+            return Directory.Exists(path) ? path : Path.GetDirectoryName(path);
+        }
+    }
+}
